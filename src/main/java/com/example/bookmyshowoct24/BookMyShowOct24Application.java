34ac5a4a7b645fc5fb413ba627500f171c761a67
deepakkasera <deepak.kasera143.dk@gmail.com>
2024-10-27 22:55:24 +0530

    BMS

--- src/main/java/com/example/bookmyshowoct24/BookMyShowOct24Application.java
+++ src/main/java/com/example/bookmyshowoct24/BookMyShowOct24Application.java
@@ -2,8 +2,10 @@ package com.example.bookmyshowoct24;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookMyShowOct24Application {
 
     public static void main(String[] args) {
